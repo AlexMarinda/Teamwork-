115c6b02a80f917702686c8f0ff259356175172f
System Administrator <root@alex-MacBook.local>
2019-09-06 07:35:56 +0200

    Create a user account Create a user account [finishes #168198573]

---
+++ UI/js/main.js
@@ -0,0 +1,164 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+  }
+
+  function myFunction3() {
+   
+    var a = document.getElementById("myDIV1");
+
+   
+
+
+    if (a.style.display === "none") {
+      a.style.display = "block";
+    } else {
+      a.style.display = "none";
+    }
+
+
+  }
+
+
+  function myFunction4() {
+   
+    var b = document.getElementById("myDIV2");
+
+    if (b.style.display === "none") {
+      b.style.display = "block";
+    } else {
+      b.style.display = "none";
+    }
+
+
+  }
+
+
+  function myFunction5() {
+   
+    var c = document.getElementById("myDIV3");
+
+    if (c.style.display === "none") {
+      c.style.display = "block";
+    } else {
+      c.style.display = "none";
+    }
+
+
+  }
+
+
+  function myFunction6() {
+   
+    var d = document.getElementById("myDIV4");
+
+    if (d.style.display === "none") {
+      d.style.display = "block";
+    } else {
+      d.style.display = "none";
+    }
+
+
+  }
+
+  function myFunction7() {
+   
+    var e = document.getElementById("myDIV5");
+
+    if (e.style.display === "none") {
+      e.style.display = "block";
+    } else {
+      e.style.display = "none";
+    }
+
+
+  }
+
+  function myFunction8() {
+   
+    var f = document.getElementById("myDIV6");
+
+    if (f.style.display === "none") {
+      f.style.display = "block";
+    } else {
+      f.style.display = "none";
+    }
+
+  }
+
+  function myFunction9() {
+   
+    var g = document.getElementById("myDIV7");
+
+    if (g.style.display === "none") {
+      g.style.display = "block";
+    } else {
+      g.style.display = "none";
+    }
+
+  }
+
+  function myFunction10() {
+   
+    var h = document.getElementById("myDIV8");
+    if (h.style.display === "none") {
+      h.style.display = "block";
+    } else {
+      h.style.display = "none";
+    }
+
+  }
+
+  function myFunction11() {
+   
+    var i = document.getElementById("myDIV9");
+    if (i.style.display === "none") {
+      i.style.display = "block";
+    } else {
+      i.style.display = "none";
+    }
+
+  }
+
+
+  function myFunction12() {
+   
+    var o = document.getElementById("myDIV10");
+
+
+
+    if (o.style.display === "none") {
+      o.style.display = "block";
+    } else {
+      o.style.display = "none";
+    }
+
+  }
+
+
+
+  function myFunction1() {
+    var x = document.getElementById("mdiv");
+   
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    function myFunction2() {
+      var x = document.getElementById("myDI");
+     
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+  }
